fa9f20ca5b87f70d2365e789a20b20bb2fba9e86
Felix Schütz <mail@felix-schuetz.de>
2021-04-09 14:53:41 +0200

    Run slow first

--- example.cpp
+++ example.cpp
@@ -16,7 +16,7 @@ simcpp20::process clock_proc(simcpp20::simulation &sim, std::string name,
 
 int main() {
   simcpp20::simulation sim;
-  clock_proc(sim, "fast", 1);
   clock_proc(sim, "slow", 2);
+  clock_proc(sim, "fast", 1);
   sim.run_until(5);
 }
