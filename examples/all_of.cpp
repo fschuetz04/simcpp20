4f0e3f433e1c0780ac2827f6ffe050c167a76ce7
Felix Schütz <mail@felix-schuetz.de>
2021-04-19 23:46:32 +0200

    Add examples for any_of and all_of

---
+++ examples/all_of.cpp
@@ -0,0 +1,20 @@
+// Copyright © 2021 Felix Schütz.
+// Licensed under the MIT license. See the LICENSE file for details.
+
+#include <cstdio>
+
+#include "simcpp20.hpp"
+
+simcpp20::event process(simcpp20::simulation &sim) {
+  printf("[%.0f] 1\n", sim.now());
+  co_await sim.all_of({sim.timeout(1), sim.timeout(2)});
+  printf("[%.0f] 2\n", sim.now());
+  co_await sim.all_of({sim.timeout(1), sim.event()});
+  printf("[%.0f] 3\n", sim.now());
+}
+
+int main() {
+  simcpp20::simulation sim;
+  process(sim);
+  sim.run();
+}
