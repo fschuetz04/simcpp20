8df1b4680bfd298bb8e4f1b47424972de0f685de
Felix Schütz <mail@felix-schuetz.de>
2021-04-21 14:39:17 +0200

    Simplify tests cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,8 +1,11 @@
 // Copyright © 2021 Felix Schütz.
 // Licensed under the MIT license. See the LICENSE file for details.
 
-#include <catch2/catch_test_macros.hpp>
+#include <algorithm>
+#include <iostream>
 
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/generators/catch_generators.hpp"
 #include "simcpp20.hpp"
 
 simcpp20::event awaiter(simcpp20::simulation &sim, simcpp20::event ev,
@@ -13,70 +16,50 @@ simcpp20::event awaiter(simcpp20::simulation &sim, simcpp20::event ev,
   finished = true;
 };
 
-TEST_CASE("any_of") {
+TEST_CASE("boolean logic") {
   simcpp20::simulation sim;
 
-  SECTION("any_of is triggered when the first event is processed") {
-    auto first_ev = sim.any_of({sim.timeout(1), sim.timeout(2)});
-    bool first_finished = false;
-    awaiter(sim, first_ev, 1, first_finished);
+  double a = GENERATE(1, 2);
+  auto ev_a = sim.timeout(a);
+  auto ev_b = sim.timeout(3 - a);
 
-    auto second_ev = sim.any_of({sim.timeout(2), sim.timeout(1)});
-    bool second_finished = false;
-    awaiter(sim, second_ev, 1, second_finished);
+  SECTION("any_of is triggered when the first event is processed") {
+    auto ev = sim.any_of({ev_a, ev_b});
+    bool finished = false;
+    awaiter(sim, ev, 1, finished);
 
     sim.run();
 
-    REQUIRE(first_finished);
-    REQUIRE(second_finished);
+    REQUIRE(finished);
   }
 
   SECTION("| is an alias for any_of") {
-    auto first_ev = sim.timeout(1) | sim.timeout(2);
-    auto first_finished = false;
-    awaiter(sim, first_ev, 1, first_finished);
-
-    auto second_ev = sim.timeout(2) | sim.timeout(1);
-    auto second_finished = false;
-    awaiter(sim, second_ev, 1, second_finished);
+    auto ev = ev_a | ev_b;
+    bool finished = false;
+    awaiter(sim, ev, 1, finished);
 
     sim.run();
 
-    REQUIRE(first_finished);
-    REQUIRE(second_finished);
+    REQUIRE(finished);
   }
-}
-
-TEST_CASE("all_of") {
-  simcpp20::simulation sim;
 
   SECTION("all_of is triggered when all events are processed") {
-    auto first_ev = sim.all_of({sim.timeout(1), sim.timeout(2)});
-    bool first_finished = false;
-    awaiter(sim, first_ev, 2, first_finished);
-
-    auto second_ev = sim.all_of({sim.timeout(2), sim.timeout(1)});
-    bool second_finished = false;
-    awaiter(sim, second_ev, 2, second_finished);
+    auto ev = sim.all_of({ev_a, ev_b});
+    bool finished = false;
+    awaiter(sim, ev, 2, finished);
 
     sim.run();
 
-    REQUIRE(first_finished);
-    REQUIRE(second_finished);
+    REQUIRE(finished);
   }
 
   SECTION("& is an alias for any_of") {
-    auto first_ev = sim.timeout(1) & sim.timeout(2);
-    auto first_finished = false;
-    awaiter(sim, first_ev, 2, first_finished);
-
-    auto second_ev = sim.timeout(2) & sim.timeout(1);
-    auto second_finished = false;
-    awaiter(sim, second_ev, 2, second_finished);
+    auto ev = ev_a & ev_b;
+    bool finished = false;
+    awaiter(sim, ev, 2, finished);
 
     sim.run();
 
-    REQUIRE(first_finished);
-    REQUIRE(second_finished);
+    REQUIRE(finished);
   }
 }
