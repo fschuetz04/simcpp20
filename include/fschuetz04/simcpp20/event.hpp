446b3f164aef6d3aa714c671175752701cedf1ba
Felix Schütz <mail@felix-schuetz.de>
2021-05-06 15:20:52 +0200

    Update event documentation

--- include/fschuetz04/simcpp20/event.hpp
+++ include/fschuetz04/simcpp20/event.hpp
@@ -6,53 +6,53 @@
 #include <cassert>    // assert
 #include <cmath>      // std::log2
 #include <coroutine>  // std::coroutine_handle, std::suspend_never
+#include <cstddef>    // std::size_t
 #include <functional> // std::function
-#include <optional>   // std::optional, std::nullopt
+#include <functional> // std::hash
+#include <utility>    // std::exchange
 #include <vector>     // std::vector
 
 namespace simcpp20 {
 template <typename Time> class simulation;
 
 /**
- * Can be awaited by processes.
+ * One event.
  *
  * @tparam Time Type used for simulation time.
  */
 template <typename Time = double> class event {
 public:
-  class promise_type;
-
   /**
-   * Construct a new pending event.
+   * Constructor.
    *
-   * @param simulation Reference to simulation.
+   * @param simulation Reference to the simulation.
    */
   explicit event(simulation<Time> &sim) : data_{new data(sim)} {
     data_->use_count_ += 1;
   }
 
-  /// Destruct this event.
+  /// Destructor.
   ~event() { decrement_use_count(); }
 
   /**
-   * Copy construct an event.
+   * Copy constructor.
    *
    * @param other Event to copy.
    */
   event(const event &other) : data_{other.data_} { data_->use_count_ += 1; }
 
   /**
-   * Move construct an event.
+   * Move constructor.
    *
    * @param other Event to move.
    */
   event(event &&other) noexcept : data_{std::exchange(other.data_, nullptr)} {}
 
   /**
-   * Replace this event by copying.
+   * Copy assignment operator.
    *
    * @param other Event to replace this event with.
-   * @return Reference to this event.
+   * @return Reference to this instance.
    */
   event &operator=(const event &other) {
     decrement_use_count();
@@ -62,10 +62,10 @@ public:
   }
 
   /**
-   * Replace this event by moving.
+   * Move assignment operator.
    *
    * @param other Event to replace this event with.
-   * @return Reference to this event.
+   * @return Reference to this instance.
    */
   event &operator=(event &&other) noexcept {
     decrement_use_count();
@@ -75,9 +75,7 @@ public:
 
   /**
    * Set the event state to triggered and schedule it to be processed
-   * immediately.
-   *
-   * If the event is not pending, nothing is done.
+   * immediately. If the event is not pending, nothing is done.
    *
    * TODO(fschuetz04): Check whether used on a process?
    */
@@ -91,9 +89,8 @@ public:
   }
 
   /**
-   * Set the event state to aborted and destroy all coroutines waiting for it.
-   *
-   * If the event is not pending, nothing is done.
+   * Set the event state to aborted. If the event is not pending, nothing is
+   * done.
    *
    * TODO(fschuetz04): Check whether used on a process? Destroy process
    * coroutine?
@@ -113,11 +110,7 @@ public:
     data_->cbs_.clear();
   }
 
-  /**
-   * Add a callback to the event to be called when the event is processed.
-   *
-   * @param cb Callback to add to the event.
-   */
+  /// @param cb Callback to be called when the event is processed.
   void add_callback(std::function<void(const event<Time> &)> cb) const {
     if (processed() || aborted()) {
       return;
@@ -141,13 +134,15 @@ public:
   bool aborted() const { return data_->state_ == state::aborted; }
 
   /**
-   * @return Whether the event is already processed and a waiting coroutine must
-   * not be paused.
+   * Called when using co_await on the event. The calling coroutine is only
+   * suspended if this method returns false.
+   *
+   * @return Whether the event is processed.
    */
   bool await_ready() const { return processed(); }
 
   /**
-   * Suspend a coroutine and resume it when the event is processed.
+   * Called when a coroutine is suspended after using co_await on the event.
    *
    * @tparam Promise Promise type of the coroutine.
    * @param handle Corotuine handle.
@@ -165,7 +160,7 @@ public:
     awaiting_ev_ = &handle.promise().ev_;
   }
 
-  /// Resume a process.
+  /// Called when a coroutine is resumed after using co_await on the event.
   void await_resume() {
     data_->use_count_ += 1;
     auto awaiting_ev = std::exchange(awaiting_ev_, nullptr);
@@ -176,39 +171,44 @@ public:
   }
 
   /**
-   * Alias for sim.any_of. Create a pending event which is triggered when this
-   * event or the given event is processed.
+   * Alias for simulation::any_of.
    *
-   * @param other Given event.
-   * @return Created event.
+   * @param other Other event.
+   * @return New pending event which is triggered when this event or the other
+   * event is processed.
    */
   event<Time> operator|(const event<Time> &other) const {
     return data_->sim_.any_of({*this, other});
   }
 
   /**
-   * Alias for sim.all_of. Create a pending event which is triggered when this
-   * event and the given event is processed.
+   * Alias for simulation::all_of.
    *
-   * @param other Given event.
-   * @return Created event.
+   * @param other Other event.
+   * @return New pending event which is triggered when this event and the other
+   * event are processed.
    */
   event<Time> operator&(const event<Time> &other) const {
     return data_->sim_.all_of({*this, other});
   }
 
+  /** Comparison operator.
+   *
+   * @param other Other event.
+   * @return Whether this event is equal to the other event.
+   */
   bool operator==(const event<Time> &other) const {
     return data_ == other.data_;
   }
 
-  /// Promise type for process coroutines.
+  /// Promise type for a coroutine returning an event.
   class promise_type {
   public:
     /**
-     * Construct a new promise type instance.
+     * Constructor.
      *
-     * @tparam Args Additional arguments passed to the process function. These
-     * arguments are ignored.
+     * @tparam Args Types of additional arguments passed to the coroutine
+     * function.
      * @param sim Reference to the simulation.
      */
     template <typename... Args>
@@ -216,12 +216,12 @@ public:
         : sim_{sim}, ev_{sim} {}
 
     /**
-     * Construct a new promise type instance.
+     * Constructor.
      *
-     * @tparam Class Class instance if the process function is a lambda or a
-     * member function of a class. This argument is ignored.
-     * @tparam Args Additional arguments passed to the process function. These
-     * arguments are ignored.
+     * @tparam Class Class type if the coroutine function is a lambda or a
+     * member function of a class.
+     * @tparam Args Types of additional arguments passed to the coroutine
+     * function.
      * @param sim Reference to the simulation.
      */
     template <typename Class, typename... Args>
@@ -229,58 +229,69 @@ public:
         : sim_{sim}, ev_{sim} {}
 
     /**
-     * Construct a new promise type instance.
+     * Constructor.
      *
-     * @tparam Class Class instance if the process function is a lambda or a
-     * member function of a class. Must contain a member variable sim
-     * referencing the simulation.
-     * @tparam Args Additional arguments passed to the process function. These
-     * arguments are ignored.
+     * @tparam Class Class type if the coroutine function is a member function
+     * of a class. Must contain a member variable sim referencing the simulation
+     * instance.
+     * @tparam Args Types of additional arguments passed to the coroutine
+     * function.
      * @param c Class instance.
      */
     template <typename Class, typename... Args>
     explicit promise_type(Class &&c, Args &&...) : sim_{c.sim}, ev_{c.sim} {}
 
 #ifdef __INTELLISENSE__
-    /**
-     * Fix IntelliSense complaining about missing default constructor for the
-     * promise type.
-     */
+    // IntelliSense fix. See https://stackoverflow.com/q/67209981.
     promise_type();
 #endif
 
-    /// @return Event which will be triggered when the process finishes.
+    /**
+     * Called to get the return value of the coroutine function.
+     *
+     * @return Event associated with the coroutine. This event is triggered
+     * when the coroutine returns.
+     */
     event<Time> get_return_object() const { return ev_; }
 
     /**
-     * Register the process to be started immediately via an initial event.
+     * Called when the coroutine is started. The coroutine awaits the return
+     * value before running.
      *
-     * @return Initial event.
+     * @return Event which will be processed at the current simulation time.
      */
     event<Time> initial_suspend() const { return sim_.timeout(Time{0}); }
 
-    /// @return Awaitable which is always ready.
-    std::suspend_never final_suspend() const noexcept { return {}; }
-
-    /// No-op.
+    /// Called when an exception is thrown inside the coroutine and not handled.
     void unhandled_exception() const { assert(false); }
 
-    /// Trigger the underlying event since the process finished.
+    /**
+     * Called when the coroutine returns. Trigger the event associated with the
+     * coroutine.
+     */
     void return_void() const { ev_.trigger(); }
+
+    /**
+     * Called after the coroutine returns.
+     *
+     * @return Awaitable which is always ready.
+     */
+    std::suspend_never final_suspend() const noexcept { return {}; }
 
     /// Refernece to the simulation.
     simulation<Time> &sim_;
 
-    /// Underlying event which is triggered when the process finishes.
+    /**
+     * Event associated with the coroutine. This event is triggered when the
+     * coroutine returns.
+     */
     event<Time> ev_;
   };
 
 private:
   /**
-   * Process the event.
-   *
-   * Set the event state to processed. Call all callbacks for this event.
-   * Resume all coroutines waiting for this event.
+   * Set the event state to processed, resume all coroutines awaiting this
+   * event, and call all callbacks added to the event.
    */
   void process() const {
     if (processed() || aborted()) {
@@ -300,7 +311,10 @@ private:
     data_->cbs_.clear();
   }
 
-  /// Decrement the use count and free the shared state if it reaches 0.
+  /**
+   * Decrement the use count of the shared data and delete it if the use count
+   * reaches 0.
+   */
   void decrement_use_count() {
     if (awaiting_ev_ != nullptr || data_ == nullptr) {
       return;
@@ -312,17 +326,13 @@ private:
     }
   }
 
-  /// State of an event.
+  /// State of the event.
   enum class state {
-    /**
-     * Event is not yet triggered or aborted. This is the initial state of a new
-     * event.
-     */
+    /// Event is not yet triggered or aborted.
     pending,
 
-    /**
-     * Event is triggered and will be processed at the current simulation time.
-     */
+    /// Event is triggered and will be processed at the current simulation
+    /// time.
     triggered,
 
     /// Event is processed.
@@ -335,26 +345,30 @@ private:
   /// Shared data of the event.
   class data {
   public:
-    /// Construct a new shared data instance.
+    /**
+     * Constructor.
+     *
+     * @param sim Reference to the simulation.
+     */
     explicit data(simulation<Time> &sim) : sim_{sim} {}
 
-    /// Destroy all coroutines still waiting for the event.
+    /// Destructor.
     ~data() {
       for (auto &handle : handles_) {
         handle.destroy();
       }
     }
 
-    /// Use count of the event.
+    /// Use count of the shared data.
     int use_count_ = 0;
 
     /// State of the event.
     state state_ = state::pending;
 
-    /// Coroutine handles of processes waiting for this event.
+    /// Handles of coroutines awaiting the event.
     std::vector<std::coroutine_handle<>> handles_ = {};
 
-    /// Callbacks for this event.
+    /// Callbacks added to the event.
     std::vector<std::function<void(const event<Time> &)>> cbs_ = {};
 
     /// Reference to the simulation.
@@ -367,19 +381,22 @@ private:
   /// Shared data of the event.
   data *data_;
 
-  /// The simulation needs access to process.
   friend class simulation<Time>;
-
-  /// std::hash needs access to data_.
   friend class std::hash<event<Time>>;
 };
 } // namespace simcpp20
 
 namespace std {
+/// Spezialization of std::hash for simcpp20::event.
 template <typename Time> struct hash<simcpp20::event<Time>> {
-  size_t operator()(const simcpp20::event<Time> &ev) const {
-    static const size_t shift = std::log2(1 + sizeof(simcpp20::event<Time>));
-    return reinterpret_cast<size_t>(ev.data_) >> shift;
+  /**
+   * @param ev Event.
+   * @return Hash of the event.
+   */
+  std::size_t operator()(const simcpp20::event<Time> &ev) const {
+    static const std::size_t shift =
+        std::log2(1 + sizeof(simcpp20::event<Time>));
+    return reinterpret_cast<std::size_t>(ev.data_) >> shift;
   }
 };
 } // namespace std
