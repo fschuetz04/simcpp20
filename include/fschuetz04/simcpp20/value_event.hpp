ef5de209c5a3808511ddf51c16a2bd29f14f2db9
Felix Schuetz <mail@felix-schuetz.de>
2021-05-07 17:34:30 +0200

    Add comments back to value_event

--- include/fschuetz04/simcpp20/value_event.hpp
+++ include/fschuetz04/simcpp20/value_event.hpp
@@ -10,6 +10,12 @@
 #include <utility>   // std::forward, std::exchange
 
 namespace simcpp20 {
+/**
+ * One event with a value.
+ *
+ * @tparam Time Type used for simulation time.
+ * @tparam Value Type of the value.
+ */
 template <typename Value, class Time = double>
 class value_event : public event<Time> {
 public:
@@ -20,6 +26,15 @@ public:
    */
   explicit value_event(simulation<Time> &sim) : event<Time>{new data(sim)} {}
 
+  /**
+   * Set the event state to triggered, and schedule it to be processed
+   * immediately. If the event is not pending, nothing is done.
+   *
+   * TODO(fschuetz04): Check whether used on a process?
+   *
+   * @tparam Types of arguments to construct the event value with.
+   * @param args Arguments to construct the event value with.
+   */
   template <typename... Args> void trigger(Args &&...args) const {
     if (!event<Time>::pending()) {
       return;
@@ -29,26 +44,61 @@ public:
     event<Time>::trigger();
   }
 
+  /**
+   * Called when a coroutine is resumed after using co_await on the event or if
+   * the coroutine did not need to be suspended. The return value is the return
+   * value of the co_await expression.
+   *
+   * @return Value of the event.
+   */
   Value &await_resume() {
     event<Time>::await_resume();
     return value();
   }
 
+  /// @return Value of the event.
   Value &value() const {
     auto casted_data = static_cast<data *>(event<Time>::data_);
     return *casted_data->value_;
   }
 
+  /// Promise type for a coroutine returning a value event.
   class promise_type {
   public:
+    /**
+     * Constructor.
+     *
+     * @tparam Args Types of additional arguments passed to the coroutine
+     * function.
+     * @param sim Reference to the simulation.
+     */
     template <typename... Args>
     explicit promise_type(simulation<Time> &sim, Args &&...)
         : sim_{sim}, ev_{sim} {}
 
+    /**
+     * Constructor.
+     *
+     * @tparam Class Class type if the coroutine function is a lambda or a
+     * member function of a class.
+     * @tparam Args Types of additional arguments passed to the coroutine
+     * function.
+     * @param sim Reference to the simulation.
+     */
     template <typename Class, typename... Args>
     explicit promise_type(Class &&, simulation<Time> &sim, Args &&...)
         : sim_{sim}, ev_{sim} {}
 
+    /**
+     * Constructor.
+     *
+     * @tparam Class Class type if the coroutine function is a member function
+     * of a class. Must contain a member variable sim referencing the simulation
+     * instance.
+     * @tparam Args Types of additional arguments passed to the coroutine
+     * function.
+     * @param c Class instance.
+     */
     template <typename Class, typename... Args>
     explicit promise_type(Class &&c, Args &&...) : sim_{c.sim}, ev_{c.sim} {}
 
@@ -57,20 +107,50 @@ public:
     promise_type();
 #endif
 
+    /**
+     * Called to get the return value of the coroutine function.
+     *
+     * @return Value event associated with the coroutine. This event is
+     * triggered when the coroutine returns with the value it returns.
+     */
     value_event<Value, Time> get_return_object() const { return ev_; }
 
+    /**
+     * Called when the coroutine is started. The coroutine awaits the return
+     * value before running.
+     *
+     * @return Event which will be processed at the current simulation time.
+     */
     event<Time> initial_suspend() const { return sim_.timeout(Time{0}); }
 
-    std::suspend_never final_suspend() const noexcept { return {}; }
-
+    /// Called when an exception is thrown inside the coroutine and not handled.
     void unhandled_exception() const { assert(false); }
 
+    /**
+     * Called when the coroutine returns. Trigger the event associated with the
+     * coroutine with the value returned by the coroutine.
+     *
+     * @tparam Types of arguments to construct the return value with.
+     * @parma Arguments to construct the return value with.
+     */
     template <typename... Args> void return_value(Args &&...args) const {
       ev_.trigger(std::forward<Args>(args)...);
     }
 
+    /**
+     * Called after the coroutine returns.
+     *
+     * @return Awaitable which is always ready.
+     */
+    std::suspend_never final_suspend() const noexcept { return {}; }
+
+    /// Reference to the simulation.
     simulation<Time> &sim_;
 
+    /**
+     * Value event associated with the coroutine. This event is triggered when
+     * the coroutine returns with the value the coroutine returns.
+     */
     value_event<Value, Time> ev_;
   };
 
@@ -80,6 +160,7 @@ private:
   public:
     using event<Time>::data::data;
 
+    /// Destructor.
     ~data() override {
       event<Time>::data::~data();
 
@@ -92,6 +173,10 @@ private:
     Value *value_;
   };
 
+  /**
+   * @tparam Args Types of arguments to construct the event value with.
+   * @param args Arguments to construct the event value with.
+   */
   template <typename... Args> void set_value(Args &&...args) const {
     auto casted_data = static_cast<data *>(event<Time>::data_);
     casted_data->value_ = new Value(std::forward<Args>(args)...);
